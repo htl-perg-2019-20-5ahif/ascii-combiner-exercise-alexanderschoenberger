61c364821341f996e980570eef74cf4416706418
Alexander Schoenberger <20150068@HTBLAPerg.onmicrosoft.at>
2019-10-10 09:45:47 +0200

    add

---
+++ Ascii Combiner/Ascii Combiner/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ascii_Combiner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length >= 3 && args[0].Equals("run"))
+            {
+                var library = new Library.Library();
+                List<string[]> files = ReadFile(args.Skip(1).ToArray());
+                if (files == null)
+                    return 1;
+                foreach (string[] test in files)
+                {
+                    PrintArray(test);
+                }
+                if (library.CheckFiles(files))
+                {
+                    string[] combined = library.CombineFiles(files);
+                    PrintArray(combined);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: The file have no equals sizes");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR: To less arguments");
+            }
+            return 0;
+        }
+
+        private static void PrintArray(string[] combined)
+        {
+            Console.WriteLine();
+            foreach (string line in combined)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        private static List<string[]> ReadFile(string[] fileNames)
+        {
+            List<string[]> list = new List<string[]>();
+            foreach (string file in fileNames)
+            {
+                try
+                {
+                    list.Add(System.IO.File.ReadAllLines(file, Encoding.ASCII));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ Ascii Combiner/Library/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Library
+{
+    public class Library
+    {
+        public bool CheckFiles(List<string[]> files)
+        {
+            int length = files[0].Length;
+            int height = files[0][0].Length;
+            foreach (string[] file in files)
+            {
+                if (file.Length != length)
+                {
+                    return false;
+                }
+                foreach (string line in file)
+                {
+                    if (line.Length != height)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public string[] CombineFiles(List<string[]> files)
+        {
+            string[] combined = files[0];
+            foreach (string[] file in files.Skip(1))
+            {
+                for (int i = 0; i < combined.Length; i++)
+                {
+                    char[] newLine = file[i].ToCharArray();
+                    char[] oldLine = combined[i].ToCharArray();
+                    for (int j = 0; j < newLine.Length; j++)
+                    {
+                        if (' ' != newLine[j])
+                        {
+                            oldLine[j] = newLine[j];
+                        }
+                    }
+                    combined[i] = new String(oldLine);
+                }
+            }
+            return combined;
+        }
+    }
+}
